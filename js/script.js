ae5691f74f53e150f3206dcb3b187ce6aa022ab3
victoria139 <99456545+victoria139@users.noreply.github.com>
2022-02-17 06:37:09 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 <script>
 //Enable hidden nav bar
-{const nav = document.querySelector(".nav");
+{const nav= document.querySelector(".header-nav--hidden");
 let last ScrollY = window.scrollY;
 window.addEventListener("scroll",() =>{
   if (lastScrollY<window,scrollY) {
