8c4aef998ec1f49a73bd09b2c4f5ae199578e310
victoria139 <99456545+victoria139@users.noreply.github.com>
2022-02-17 05:37:33 -0500

    Update script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-var prev = 0;
-var $window = $(window);
-var nav = $('.nav'); 
-$window.on('scroll', function(){
-  var scrollTop = $window.scrollTop();
-  nav.toggleClass('hidden', scrollTop > prev);
-  prev = scrollTop;
-});
